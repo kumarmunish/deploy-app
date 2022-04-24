73b9b42bd72d214ff7d48bad6a9ab6c3595d7445
Munish <munish.kumar@shuttl.com>
2022-04-25 00:08:57 +0530

    use terraform s3 backend

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,15 @@
 terraform {
   required_version = "= 1.1.9"
+
+# comment this code and run terraform plan/apply and then do terraform init again to use this backend 
+  backend "s3" {
+    bucket = "tfstate-bucket-{unique-identifier}"
+    key    = "terraform.tfstate"
+    region = "ap-southeast-2"
+    aws_dynamodb_table = "terraform_state_locking"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
